e1747d3fe3e0974e4f7a47666dc19e29fc44282f
Gian-Marco Coray <gianmarcocoray@gmail.com>
2025-08-17 14:21:11 +0200

    properly added license back

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 Gian-Marco Coray
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // Contexts
 import { LanguageProvider } from './src/context/LanguageContext';
 import { ThemeProvider } from './src/context/ThemeContext';
